108fa5ff5e27a2c1d72f5f248d7a85717bb6f1fe
Sharay0607 <smachacon@unimagdalena.edu.co>
2026-09-20 18:15:05 -0500

    crea archivos y los dominio de (EventCategory, EventStatus, TicketType, TicketStatus)

---
+++ src/main/java/com/pulsepass/domain/EventCategory.java
@@ -0,0 +1,5 @@
+package com.pulsepass.domain;
+
+public enum EventCategory {
+    MUSIC, SPORTS, TECHNOLOGY, EDUCATION, CULTURE, ENTERTAINMENT
+}
---
+++ src/main/resources/db/migration/V2__insert_initial_artists.sql
@@ -0,0 +1,7 @@
+INSERT INTO artists (stage_name, country, genre, active) VALUES
+    ('Solar Beat', 'Colombia', 'Electronic', TRUE),
+    ('Neon Waves', 'Mexico', 'Synthpop', TRUE),
+    ('Caribbean Sound', 'Colombia', 'Reggae', TRUE),
+    ('Ocean Drive', 'USA', 'Pop', TRUE),
+    ('Digital Pulse', 'Argentina', 'Techno', TRUE);
+    
